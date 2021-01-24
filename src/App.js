6dc54e444709ea19621dcb1327f21f0e0aadc805
browncow5 <ryan.e.donat@gmail.com>
2021-01-24 17:18:31 +0000

    add basic list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+ 
+class Item extends Component {
+  state = {
+    listitems: [
+      {
+        id: 0,
+        context: "Primary",
+        modifier: "list-group-item list-group-item-primary"
+      },
+      {
+        id: 1,
+        context: "Secondary",
+        modifier: "list-group-item list-group-item-secondary"
+      },
+      {
+        id: 2,
+        context: "Success",
+        modifier: "list-group-item list-group-item-success"
+      },
+      {
+        id: 3,
+        context: "Danger",
+        modifier: "list-group-item list-group-item-danger"
+      },
+      {
+        id: 4,
+        context: "Warning",
+        modifier: "list-group-item list-group-item-warning"
+      }
+    ]
+  };
+ 
+  render() {
+    return (
+      <React.Fragment>
+        <ul className="list-group">
+          {this.state.listitems.map(listitem => (
+            <li key={listitem.id} className={listitem.modifier}>
+              {listitem.context}
+            </li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
+}
 
 function App() {
   return (
@@ -18,6 +65,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Item></Item>
     </div>
   );
 }
